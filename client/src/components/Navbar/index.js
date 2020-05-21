88999dbd5c32a3cf6650b1e91aff2997aa2acddb
Nathan Sartain <nathanmsartain@gmail.com>
2020-05-21 14:17:02 -0400

    Finised front end input validator and /register form.

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar as BootNav, Nav } from 'react-bootstrap';
 import './style.css';
