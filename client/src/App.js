c0549a4d8e70f818149a0425f9b3ea6c30f7ecf8
Nathan Sartain <nathanmsartain@gmail.com>
2020-05-21 09:15:57 -0400

    Added home page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import {
-  BrowserRouter as Router
+  BrowserRouter as Router,
+  Route,
+  Switch
 } from 'react-router-dom';
 import Navbar from './components/Navbar';
-
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </Router>
   );
 }
