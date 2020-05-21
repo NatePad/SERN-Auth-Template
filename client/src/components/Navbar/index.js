f0aa95ac12db651564d8fbaeb67ca2c47d23d23a
Nathan Sartain <nathanmsartain@gmail.com>
2020-05-21 10:32:54 -0400

    Added dynamic active class to navbar links.

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar as BootNav, Nav } from 'react-bootstrap';
 import './style.css';
 
 
 const Navbar = props => {
+  const [loc, setLoc] = useState(window.location.pathname);
 
   return (
     <BootNav bg="light" expand="lg">
 
-      <Link to="/" className="navbar-brand">
+      <Link to="/" className="navbar-brand" onClick={() => setLoc('/')}>
         <i className="fas fa-home"></i> Home
       </Link>
 
@@ -17,19 +18,45 @@ const Navbar = props => {
       <BootNav.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
 
-          <Link to="/profile" className="nav-link">
+          <Link
+            to="/profile"
+            className={
+              loc === '/profile'
+                ? 'nav-link active'
+                : 'nav-link'
+            }
+            onClick={() => setLoc('/profile')}
+          >
             <i className="fas fa-user-lock"></i> Profile
           </Link>
 
-          <Link to="/register" className="nav-link">
+          <Link
+            to="/register"
+            className={
+              loc === '/register'
+                ? 'nav-link active'
+                : 'nav-link'}
+            onClick={() => setLoc('/register')}
+          >
             <i className="fas fa-user-plus"></i> Register
           </Link>
 
-          <Link to="/login" className="nav-link">
+          <Link
+            to="/login"
+            className={loc === '/login'
+              ? 'nav-link active'
+              : 'nav-link'
+            }
+            onClick={() => setLoc('/login')}
+          >
             <i className="fas fa-sign-in-alt"></i> Sign In
           </Link>
 
-          <Link to="/" className="nav-link">
+          <Link
+            to="/"
+            className="nav-link"
+            onClick={() => setLoc('/')}
+          >
             <i className="fas fa-sign-out-alt"></i> Sign Out
           </Link>
 
