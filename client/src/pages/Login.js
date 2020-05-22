fd579fec936ab0d297b8044c3aef030626d68cef
Nathan Sartain <nathanmsartain@gmail.com>
2020-05-22 12:08:10 -0400

    Framed /login form.

---
+++ client/src/pages/Login.js
@@ -0,0 +1,51 @@
+import React, { createRef } from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+
+const Login = props => {
+  const email = createRef();
+  const password = createRef();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const userData = {
+      email: email.current.value,
+      password: password.current.value
+    }
+
+    // console.log(userData);
+  }
+
+  return (
+    <Container>
+
+      <Form id="login-form" onSubmit={handleSubmit}>
+
+        <Form.Group controlId="email">
+          <Form.Label>Email Address:</Form.Label>
+          <Form.Control
+            name="email"
+            placeholder="your@email.com"
+            ref={email}
+            type="email"
+          />
+        </Form.Group>
+
+        <Form.Group controlId="password">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            name="password"
+            placeholder="Enter Password"
+            ref={password}
+            type="password"
+          />
+        </Form.Group>
+
+        <Button type="submit" variant="primary">Sign In</Button>
+      </Form>
+
+    </Container>
+  );
+}
+
+export default Login;
