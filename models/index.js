c71a61529a4834df47ccfff89337b7e9244942f7
Nathan Sartain <nathanmsartain@gmail.com>
2020-05-22 08:28:11 -0400

    Added Sequelize and User model.

---
+++ models/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+require('dotenv').config();
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const db = {};
+
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize({
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    operatorsAliases: 0
+  })
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 'use strict';
 
 const app = require('./app');
+const db = require('./models');
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
+// Manually toggle force to true in order to drop
+// and rebuild all tables in the database
+const force = false;
+db.sequelize.sync({ force }).then(() => {
+  app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
+});
