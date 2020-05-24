0b7307d0da374323ce8aa8cc5969a1664ac5688f
Nathan Sartain <nathanmsartain@gmail.com>
2020-05-24 06:57:21 -0400

    Converted /profile page to a form.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,17 +1,44 @@
-import React, { useContext } from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
 import UserContext from '../utils/UserContext';
 
 const Profile = props => {
-  const {userState} = useContext(UserContext)
+  const {userState} = useContext(UserContext);
+  const [readOnly, setReadOnly] = useState(true);
+  const [username, setUsername] = useState(userState.username);
+  const [email, setEmail] = useState(userState.email);
+
   return (
     <Container>
       <h2>Your Profile Information:</h2>
       <hr />
-      <ul>
-        <li><strong>Username:</strong> {userState.username}</li>
-        <li><strong>Email Address:</strong> {userState.email}</li>
-      </ul>
+      <Form id="profile-form">
+
+        <Form.Group controlId="username">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            name="username"
+            onChange={e => setUsername(e.target.value)}
+            plaintext={readOnly}
+            readOnly={readOnly}
+            type="text"
+            value={username}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="email">
+          <Form.Label>Email Address:</Form.Label>
+          <Form.Control
+            name="email"
+            onChange={e => setEmail(e.target.value)}
+            plaintext={readOnly}
+            readOnly={readOnly}
+            type="email"
+            value={email}
+          />
+        </Form.Group>
+        <Button variant="primary" onClick={() => setReadOnly(!readOnly)}>Edit Profile</Button>
+      </Form>
     </Container>
   );
 }
