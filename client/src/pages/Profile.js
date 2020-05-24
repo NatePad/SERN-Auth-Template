a2b50209c1a49589143c252018eaa1f6a2c6f447
Nathan Sartain <nathanmsartain@gmail.com>
2020-05-24 09:36:41 -0400

    Added /profile password modal.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
+import React, { createRef, useContext, useState } from 'react';
+import { Button, Container, Form, Modal } from 'react-bootstrap';
 import UserContext from '../utils/UserContext';
 
 const Profile = props => {
@@ -7,12 +7,37 @@ const Profile = props => {
   const [readOnly, setReadOnly] = useState(true);
   const [username, setUsername] = useState(userState.username);
   const [email, setEmail] = useState(userState.email);
+  const [modalShow, setModalShow] = useState(false);
+  const password = createRef();
+
+  const resetForm = () => {
+    setUsername(userState.username);
+    setEmail(userState.email);
+    setReadOnly(true);
+  }
+
+  const submitProfile = e => {
+    e.preventDefault();
+    if (username === userState.username && email === userState.email) return;
+    setModalShow(true);
+  }
+
+  const submitPassword = e => {
+    e.preventDefault();
+    const userData = {
+      username: username.trim(),
+      email: email.trim(),
+      password: password.current.value
+    }
+
+    console.log(userData);
+  }
 
   return (
     <Container>
       <h2>Your Profile Information:</h2>
       <hr />
-      <Form id="profile-form">
+      <Form id="profile-form" onSubmit={submitProfile}>
 
         <Form.Group controlId="username">
           <Form.Label>Username:</Form.Label>
@@ -37,8 +62,36 @@ const Profile = props => {
             value={email}
           />
         </Form.Group>
-        <Button variant="primary" onClick={() => setReadOnly(!readOnly)}>Edit Profile</Button>
+
+        {readOnly ? (
+          <Button variant="primary" onClick={() => setReadOnly(false)}>Edit Profile</Button>
+        ) : (
+          <div>
+            <Button variant="success" type="submit" className="mr-3">Submit Changes</Button>
+            <Button variant="danger" onClick={resetForm}>Cancel Changes</Button>
+          </div>
+        )}
       </Form>
+
+      <Modal show={modalShow} size="lg" centered>
+        <Modal.Body>
+          <Form id="password-form" onSubmit={submitPassword}>
+
+            <Form.Group controlId="password">
+              <Form.Label>Password:</Form.Label>
+              <Form.Control
+                name="password"
+                placeholder="Enter Password"
+                ref={password}
+                type="password"
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit" className="mr-3">Submit</Button>
+            <Button variant="warning" onClick={() => setModalShow(false)}>Close</Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+
     </Container>
   );
 }
