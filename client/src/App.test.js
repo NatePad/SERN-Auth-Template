3e24e812423c7afc3f137577e4ceb30ca8cb1ea7
Nathan Sartain <nathanmsartain@gmail.com>
2020-12-17 12:54:37 -0500

    removed unused tests

--- client/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders hello world', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, World!/i);
-  expect(linkElement).toBeInTheDocument();
-});
