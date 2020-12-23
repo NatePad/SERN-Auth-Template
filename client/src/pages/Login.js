70e8fb7d00b14e56f9e2fc7618b9ec507cdf19c0
Nathan Sartain <nathanmsartain@gmail.com>
2020-12-23 13:48:38 -0500

    Frame /login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,3 +1,5 @@
+import { createRef } from 'react';
+
 import {
   Button,
   Container,
@@ -5,27 +7,49 @@ import {
 } from 'react-bootstrap';
 
 const Login = () => {
+  const email = createRef();
+  const password = createRef();
+
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const userData = {
+      email: email.current.value.trim(),
+      password: password.current.value.trim()
+    }
+
+    console.log(userData);
+  }
+
+
   return (
     <Container>
       <h1 className="mb-3">Login Page</h1>
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text>
+      <Form onSubmit={handleSubmit}>
+
+        <Form.Group controlId="email">
+          <Form.Label>Email Address:</Form.Label>
+          <Form.Control type="email" placeholder="your@email.com" ref={email} />
+          <Form.Text className="text-danger">
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+
+        <Form.Group controlId="password">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control type="password" placeholder="P@ssw0rd!" ref={password} />
+          <Form.Text className="text-danger">
+            That password is incorrect.
+          </Form.Text>
         </Form.Group>
-        
+
 
         <Button variant="primary" type="submit">
           Submit
         </Button>
+
       </Form>
     </Container>
   )
