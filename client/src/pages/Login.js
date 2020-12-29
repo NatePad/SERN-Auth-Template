5c674c9f201130fbff33d2e275598d8a0d85a480
Nathan Sartain <nathanmsartain@gmail.com>
2020-12-29 10:12:40 -0500

    Build login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,4 @@
-import { createRef } from 'react';
+import { createRef, useState } from 'react';
 
 import {
   Button,
@@ -9,8 +9,11 @@ import {
 import API from '../utils/API';
 
 const Login = () => {
+  const TEXT_RED = 'text-danger';
   const email = createRef();
+  const [emailValid, setEmailValid] = useState(true);
   const password = createRef();
+  const [passwordValid, setPasswordValid] = useState(true);
 
 
   const handleSubmit = async e => {
@@ -23,9 +26,13 @@ const Login = () => {
 
     try {
       const results = await API.login(userData);
-      console.log(results);
+      if (results.data.username) {
+        console.log(results);
+      } else {
+        setPasswordValid(false);
+      }
     } catch (err) {
-      console.log(err);
+      setEmailValid(false);
     }
   }
 
@@ -37,8 +44,13 @@ const Login = () => {
 
         <Form.Group controlId="email">
           <Form.Label>Email Address:</Form.Label>
-          <Form.Control type="email" placeholder="your@email.com" ref={email} />
-          <Form.Text className="text-danger">
+          <Form.Control
+            type="email"
+            placeholder="your@email.com"
+            ref={email}
+            onChange={() => setEmailValid(true)}
+          />
+          <Form.Text className={emailValid ? `${TEXT_RED} invisible` : TEXT_RED}>
             Our lemmings can't find that email in our system.
           </Form.Text>
         </Form.Group>
@@ -46,8 +58,13 @@ const Login = () => {
 
         <Form.Group controlId="password">
           <Form.Label>Password:</Form.Label>
-          <Form.Control type="password" placeholder="P@ssw0rd!" ref={password} />
-          <Form.Text className="text-danger">
+          <Form.Control
+            type="password"
+            placeholder="P@ssw0rd!"
+            ref={password}
+            onChange={() => setPasswordValid(true)}
+          />
+          <Form.Text className={passwordValid ? `${TEXT_RED} invisible` : TEXT_RED}>
             That password is incorrect.
           </Form.Text>
         </Form.Group>
