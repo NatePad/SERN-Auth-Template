90965cf9a9814dc0e5dd9e77ecabe60560e4e0b5
Nathan Sartain <nathanmsartain@gmail.com>
2020-12-30 09:40:08 -0500

    Add GlobalState and actions files

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,49 @@
+import { createContext, useReducer } from 'react';
+
+import {
+  LOGIN,
+  LOGOUT
+} from './actions';
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+const reducer = (state, action) => {
+
+  switch (action.type) {
+    case LOGIN:
+      return {
+        ...state,
+        user: {
+          auth: true,
+          ...action.userData
+        }
+      }
+    
+    case LOGOUT:
+      return {
+        ...state,
+        user: {
+          auth: false
+        }
+      }
+
+    default:
+      return state;
+  }
+};
+
+const StoreProvider = ({ value = [], ...props }) => {
+  const [state, dispatch] = useReducer(reducer, {
+    user: {
+      auth: false
+    }
+  });
+  return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+  return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
