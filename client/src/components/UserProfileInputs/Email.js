465db365d0e7be02476c5c3706440be260632835
Nathan Sartain <nathanmsartain@gmail.com>
2021-01-09 18:57:46 -0500

    Set Email component state data types

--- client/src/components/UserProfileInputs/Email.js
+++ client/src/components/UserProfileInputs/Email.js
@@ -9,8 +9,8 @@ const EMAIL_REGEX = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/;
 
 
 const Email = props => {
-  const [email, setEmail] = useState();
-  const [valid, setValid] = useState();
+  const [email, setEmail] = useState('');
+  const [valid, setValid] = useState(false);
 
   // VALIDATOR
   useEffect(() => {
