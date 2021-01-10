09c21d4329d3a9af444b9f99fff524c9b5635a3e
Nathan Sartain <nathanmsartain@gmail.com>
2021-01-09 19:22:07 -0500

    Refactor username form input

---
+++ client/src/components/UserProfileInputs/Username.js
@@ -0,0 +1,89 @@
+import { useEffect, useState } from 'react';
+
+import { Form } from 'react-bootstrap';
+
+import API from '../../utils/API';
+import useDebounce from '../../utils/debounceHook';
+
+// CONSTANT VARIABLES
+// USERNAME REQUIREMENTS
+const USERNAME_MAX_LEN = 35;
+const USERNAME_MIN_LEN = 6;
+const INVAL_MSG =
+  `Usernames can be ${USERNAME_MIN_LEN} to ${USERNAME_MAX_LEN} characters ` +
+  `long and can contain letters (a-z), numbers (0-9), and periods (.).`;
+const VALID_CHARS = '0123456789abcdefghijklmnopqrstuvwxyz.';
+
+
+// VALIDATOR
+const validateUsername = username => {
+  let valid = username.length >= USERNAME_MIN_LEN
+    && username.length <= USERNAME_MAX_LEN;
+
+  const lowerName = username.toLowerCase();
+  for (let i = 0; i < lowerName.length; i++) {
+    if (!VALID_CHARS.includes(lowerName.charAt(i))) valid = false;
+  }
+
+  return valid;
+}
+
+
+// COMPONENT
+const Username = props => {
+  const [username, setUsername] = useState('');
+  const [valid, setValid] = useState(false);
+  const [msgColor, setMsgColor] = useState('text-danger');
+  const [invalMsg, setInvalMsg] = useState(INVAL_MSG);
+
+  const debouncedTerm = useDebounce(username, 500);
+
+  const queryUsername = async () => {
+    const results = await API.findByUsername(username);
+    if (results.data) {
+      setValid(false);
+      setMsgColor('text-danger');
+      setInvalMsg(`The username ${username} is taken.`);
+    } else {
+      setValid(true);
+      setMsgColor('text-success');
+      setInvalMsg(`The username ${username} is available!`);
+    }
+  }
+
+  useEffect(() => {
+    if (valid) {
+      queryUsername();
+    }
+    // eslint-disable-next-line
+  }, [debouncedTerm]);
+
+  useEffect(() => {
+    setValid(validateUsername(username));
+  }, [username]);
+
+  useEffect(() => {
+    if (!valid) {
+      setInvalMsg(INVAL_MSG);
+      setMsgColor('text-danger');
+    }
+    props.setValid(valid);
+    // eslint-disable-next-line
+  }, [valid]);
+
+  return (
+    <Form.Group controlId="username">
+      <Form.Label>Username:</Form.Label>
+      <Form.Control
+        type="username"
+        placeholder="Username"
+        onChange={e => setUsername(e.target.value.trim())}
+      />
+      <Form.Text className={msgColor}>
+        {invalMsg}
+      </Form.Text>
+    </Form.Group>
+  )
+}
+
+export default Username;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -10,87 +10,21 @@ import {
 import API from '../utils/API';
 import Email from '../components/UserProfileInputs/Email';
 import Password from '../components/UserProfileInputs/Password';
-import useDebounce from '../utils/debounceHook';
+import Username from '../components/UserProfileInputs/Username';
 
 const Register = () => {
   const [validPassword, setValidPassword] = useState(false);
   const [validEmail, setValidEmail] = useState(false);
-
-  // GENERAL VARIABLES
-  const TEXT_GREEN = 'text-success';
-  const TEXT_RED = 'text-danger';
+  const [validUsername, setValidUsername] = useState(false);
 
   const [completeForm, setCompleteForm] = useState(false);
 
   const [modalShow, setModalShow] = useState(false);
   const [modalText, setModalText] = useState('');
 
-
-  // USERNAME VARIABLES
-  const USERNAME_MAX_LEN = 35;
-  const USERNAME_MIN_LEN = 6;
-  const USERNAME_INVAL_MSG =
-    `Usernames can be ${USERNAME_MIN_LEN} to ${USERNAME_MAX_LEN} characters ` +
-    `long and can contain letters (a-z), numbers (0-9), and periods (.).`;
-  const USERNAME_VALID_CHARS = '0123456789abcdefghijklmnopqrstuvwxyz.';
-
-  const [username, setUsername] = useState('');
-  const [usernameValid, setUsernameValid] = useState(false);
-  const [usernameMsg, setUsernameMsg] = useState(USERNAME_INVAL_MSG);
-  const [usernameMsgColor, setUsernameMsgColor] = useState(TEXT_RED);
-
-
-  // DEBOUNCER
-  const debouncedTerm = useDebounce(username, 500);
-
   useEffect(() => {
-    const checkUsername = async () => {
-      const results = await API.findByUsername(username);
-      if (results.data) {
-        setUsernameMsgColor(TEXT_RED);
-        setUsernameMsg(`The username ${username} is taken.`);
-      } else {
-        setUsernameMsgColor(TEXT_GREEN);
-        setUsernameMsg(`The username ${username} is available.`);
-      }
-    }
-
-    if (usernameValid) {
-      checkUsername();
-    }
-
-    // eslint-disable-next-line
-  }, [debouncedTerm]);
-
-
-  // USERNAME SECTION
-  useEffect(() => {
-    let valid = true;
-    if (username.length < USERNAME_MIN_LEN
-      || username.length > USERNAME_MAX_LEN)
-      valid = false;
-
-    const lowerName = username.toLowerCase();
-    for (let i = 0; i < lowerName.length; i++) {
-      if (!USERNAME_VALID_CHARS.includes(lowerName.charAt(i))) valid = false;
-    }
-
-    setUsernameValid(valid);
-  }, [username]);
-
-
-  useEffect(() => {
-    if (!usernameValid) {
-      setUsernameMsg(USERNAME_INVAL_MSG);
-      setUsernameMsgColor(TEXT_RED);
-    }
-    // eslint-disable-next-line
-  }, [usernameValid]);
-
-
-  useEffect(() => {
-    setCompleteForm(validEmail && validPassword && usernameValid)
-  }, [validEmail, validPassword, usernameValid]);
+    setCompleteForm(validEmail && validPassword && validUsername)
+  }, [validEmail, validPassword, validUsername]);
 
 
   // FORM SUBMISSION HANDLER
@@ -98,7 +32,7 @@ const Register = () => {
     e.preventDefault();
 
     const userData = {
-      username,
+      username: document.querySelector('#username').value.trim(),
       email: document.querySelector('#email').value.trim(),
       password: document.querySelector('#password').value.trim()
     }
@@ -125,35 +59,19 @@ const Register = () => {
 
       <Form onSubmit={handleSubmit}>
 
-
-        {/* USERNAME */}
-        <Form.Group controlId="username">
-          <Form.Label>Username:</Form.Label>
-          <Form.Control
-            type="username"
-            placeholder="Username"
-            onChange={e => setUsername(e.target.value.trim())}
-          />
-          <Form.Text className={usernameMsgColor}>
-            {usernameMsg}
-          </Form.Text>
-        </Form.Group>
-
-
+        <Username setValid={setValidUsername} />
         <Email setValid={setValidEmail} />
         <Password setValid={setValidPassword} />
-
 
         {/* SUBMIT BUTTON */}
         <Button variant="primary" type="submit" disabled={!completeForm}>
           Submit
         </Button>
-        <Form.Text className={completeForm ? `${TEXT_RED} invisible` : TEXT_RED}>
+        <Form.Text className={completeForm ? 'invisible' : 'text-danger'}>
           Please fix all form errors before submitting.
         </Form.Text>
 
       </Form>
-
 
       {/* RESPONSE MODAL */}
       <Modal show={modalShow} onHide={() => setModalShow(false)} centered>
@@ -167,7 +85,6 @@ const Register = () => {
 
         </Modal.Body>
       </Modal>
-
 
     </Container>
   )
