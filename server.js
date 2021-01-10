03c2644b1e46bddfb3f3c18391ba8a9a78df971b
Nathan Sartain <nathanmsartain@gmail.com>
2021-01-10 16:56:35 -0500

    Combine app.js and server.js

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 'use strict';
 
-const app = require('./app');
+require('dotenv').config();
+
 const db = require('./models');
+const errorHandler = require('./middleware/errorHandler');
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static files in production.
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.use(routes);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 8000;
 
 db.sequelize.sync().then(() => {
