16d4db83bf329625327e2f395147e2c72abca653
Nathan Sartain <nathanmsartain@gmail.com>
2021-01-12 09:47:01 -0500

    Convert error handler to a switch

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,17 +1,16 @@
 'use strict';
 
 const errorHandler = (err, req, res, next) => {
-
-  if (err.name === 'SequelizeValidationError') {
-    res.status(400).send('BAD_REQUEST');
-  } else if (err.name === 'SequelizeUniqueConstraintError') {
-    res.status(200).send('NON_UNIQUE');
-  } else {
+  switch (err.name) {
+    case 'SequelizeValidationError':
+      res.status(400).send('BAD_REQUEST');
+    case 'SequelizeUniqueConstraintError':
+      res.status(200).send('NON_UNIQUE');
+    default:
     // EMAIL ADMINS
     console.log('error handler default hit:', err);
     res.status(500).send('SERVER_ERROR');
   }
-
 }
 
 module.exports = errorHandler;
